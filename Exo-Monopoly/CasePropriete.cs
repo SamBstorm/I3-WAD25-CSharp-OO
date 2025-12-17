1a4f47ee0cc81d89ee03e6a34bb2f83db9db6ce4
Samuel Legrain <samuel.legrain@bstorm.be>
2025-12-17 13:55:56 +0100

    Update Exo-Monopoly - Constructeur part01

---
+++ Exo-Monopoly/CasePropriete.cs
@@ -0,0 +1,31 @@
+﻿using Exo_Monopoly.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_Monopoly
+{
+    public class CasePropriete
+    {
+
+        public string Nom { get; private set; }
+        public Couleurs Couleur { get; private set; }
+        public int Prix { get; private set; }
+        public bool EstHypotequee { get; private set; }
+        public Joueur? Proprietaire { get; private set; }
+        public CasePropriete(string nom, Couleurs couleur, int prix)
+        {
+            Nom = nom;
+            Couleur = couleur;
+            Prix = prix;
+        }
+
+        public void Acheter(Joueur acheteur)
+        {
+            //...Tant que Joueur n'est pas à jour, je ne peux pas faire la méthode...
+        }
+
+    }
+}
