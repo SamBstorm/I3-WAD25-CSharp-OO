eaa39de30bdc331e1da7fd75726895f9ef9db523
Samuel Legrain <samuel.legrain@bstorm.be>
2025-12-17 09:51:41 +0100

    Add Demo05

---
+++ Demo05/Login.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo05
+{
+    internal class Login
+    {
+        private DateTime _creationDate = DateTime.Now;
+        public int Duration
+        {
+            get {
+                DateTime now = DateTime.Now;
+                TimeSpan delta = now - _creationDate;
+                return (int)(delta.TotalMicroseconds/1000);
+            }
+        }
+        public string Email { get; set; }
+        private string _password;
+
+        public string Password
+        {
+            /* première méthode, contourner la variable
+            get {
+                return "*******";
+            }*/
+            /* deuxième méthode, rendre notre get private
+            private get
+            {
+                return _password;
+            } */
+            /* troisième méthode, c'est de ne pas mettre de get...*/
+            set {
+                value = value.Trim();
+                _password = value;
+            }
+        }
+
+        /* Exemple de propriété sans variable
+        public string Bidon
+        {
+            get {
+                return "Ceci est bidon";
+            }
+            set {
+                Console.WriteLine(value);
+            }
+        }*/
+        /* Plus besoin de la méthode car propriété définie
+        public void SetNewPassword(string password)
+        {
+            _password = password;
+        }*/
+
+        public bool CheckPassword(string email, string password)
+        {
+            return Email == email && _password == password;
+        }
+    }
+}
---
+++ Demo05/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Demo05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Login login = new Login()
+            {
+                Email = "samuel.legrain@bstorm.be"
+            };
+            Console.WriteLine(login.Duration);
+            /* Exemple de propriété sans variable
+            login.Bidon = "Toto";
+            Console.WriteLine(login.Bidon);*/
+            login.Password = "Test1234=";
+
+            /*Mot de passe inaccessible, code plus fonctionnel
+            Console.WriteLine($"Vos données de connection :\nIdentifiant : {login.email}\nMot de passe : {login._password}");*/
+
+            Console.WriteLine($"Vos données de connection :\nIdentifiant : {login.Email}");
+
+            Console.Clear();
+
+            Console.WriteLine($"Veuillez vous identifier :");
+            string email = Console.ReadLine()!;
+            Console.WriteLine($"Veuillez introduire votre mot de passe :");
+            string pwd = Console.ReadLine()!;
+            if(login.CheckPassword(email, pwd))
+            {
+                Console.WriteLine("Bienvenue!");
+            }
+            else
+            {
+                Console.WriteLine("Compte bloqué...");
+            }
+            Console.WriteLine(login.Duration);
+        }
+    }
+}
