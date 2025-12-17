37f4fa33d221ec3bde8cde551f4f44736970b8e5
Samuel Legrain <samuel.legrain@bstorm.be>
2025-12-17 11:59:38 +0100

    Add Demo06

---
+++ Demo06/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Demo06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Voiture v1 = new Voiture("1-ABC-123","Rouge","Électrique",true, 5);
+
+            Console.WriteLine(
+@$"Nous venons d'enregistrer cette voiture :
+Immatriculation : {v1.Immatriculation}
+Couleur : {v1.Couleur}
+Moteur : {v1.TypeMoteur} (Boîte {((v1.IsAutomatic)?"Automatique" : "manuelle")})
+Nombre de roues : {v1.NbRoues} roues");
+
+            Voiture v2 = new Voiture("1-DEF-123", "Bleue");
+
+            Console.WriteLine(
+@$"Nous venons d'enregistrer cette voiture :
+Immatriculation : {v2.Immatriculation}
+Couleur : {v2.Couleur}
+Moteur : {v2.TypeMoteur} (Boîte {((v2.IsAutomatic) ? "Automatique" : "manuelle")})
+Nombre de roues : {v2.NbRoues} roues");
+
+        }
+    }
+}
---
+++ Demo06/Voiture.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Demo06
+{
+    public class Voiture
+    {
+        #region variables avec propriétés
+        private string _immatriculation;
+        public string Immatriculation
+        {
+            get
+            {
+                return _immatriculation;
+            }
+            private set
+            {
+                value = value.Trim();
+                if (Regex.IsMatch(value, "^[\\dA-Z]-[A-Z]{3}-\\d{3}$"))
+                {
+                    _immatriculation = value;
+                }
+            }
+        }
+        private string _typeMoteur;
+        public string TypeMoteur
+        {
+            get
+            {
+                return _typeMoteur;
+            }
+            private set
+            {
+                _typeMoteur = value;
+                if (value == "Électrique")
+                {
+                    IsAutomatic = true;
+                }
+            }
+        }
+        #endregion
+        #region Auto-propriétés
+        public string Couleur { get; private set; }
+        public ushort NbRoues { get; private set; } 
+        public bool IsAutomatic { get; private set; }
+
+        #endregion
+
+        #region Constructeurs
+        public Voiture(string immatriculation) : this(immatriculation, "Blanche") 
+        { 
+        }
+        public Voiture(string immatriculation, string couleur) : this(immatriculation, couleur, "Inconnu")
+        {
+        }
+
+        public Voiture(string immatriculation, string couleur, string typeMoteur, bool isAutomatic = false, ushort nbRoues = 4)
+        {
+            Immatriculation = immatriculation;
+            Couleur = couleur;
+            NbRoues = nbRoues;
+            IsAutomatic = isAutomatic;
+            TypeMoteur = typeMoteur;
+        }
+        #endregion
+
+        #region Les méthodes
+
+        #endregion
+    }
+}
